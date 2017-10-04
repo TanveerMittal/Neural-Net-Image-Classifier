33a6225055e3fc700ea7abe4dceaa260b6dc8a74
Tanveer Mittal <tanveer@mittals.us>
2017-10-04 11:54:03 -0500

    Resizing of images to 300x168

--- model/resize_shoes.py
+++ model/resize_shoes.py
@@ -3,7 +3,7 @@ import cv2
 import glob
 
 count = 0
-for x in glob.glob("shoes/*"):
+for x in glob.glob("shoes2/*"):
     image = cv2.imread(x)
-    cv2.imwrite("shoes/image%d.jpg" % count, imutils.resize(image, width=1280, height=720))
+    cv2.imwrite("resized/image%d.jpg" % count, imutils.resize(image, width=1280, height=720))
     count += 1
